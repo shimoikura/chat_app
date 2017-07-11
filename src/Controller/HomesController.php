79ea8d0795678927ab1e8d8808559998888ed72a
atm0016 <atm0016@example.com>
2017-07-11 20:48:50 +0530

    Created post page

--- src/Controller/HomesController.php
+++ src/Controller/HomesController.php
@@ -12,13 +12,41 @@ class HomesController extends AppController
 
   public function index()
   {
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    // after login
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     $this->request->session();
     $userid = $this->request->session()->read('userid');
     if ($userid == null) {
       return $this->redirect(['controller'=>'Users','action'=>'login']);
     }
+
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    // Load Contenstable and Users to Set contents
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    $this->loadModel('Contents');
+    $this->loadModel('Users');
+    $contents = $this->paginate($this->Contents);
+
+
+    $allcontents = array();
+    foreach ($contents as $value) {
+      $query = $this->Users->find()->where(['id'=>$value['userId']])->select('username');
+      $data = $query->toArray();
+      foreach ($data as $user) {
+        array_push($allcontents,array(
+          'id' => $value['id'],
+          'username' => $user['username'],
+          'body' => $value['body'],
+          'favo' => $value['favo'],
+          'created' => $value['createdDate']->format('Y/m/d H:i')
+        ));
+      }
+    }
+    // echo "<pre>";
+    // print_r($allcontents);
+    // exit();
+    $this->set('contents',$allcontents);
   }
 }
-
-
  ?>
--- src/Template/Homes/index.ctp
+++ src/Template/Homes/index.ctp
@@ -4,8 +4,24 @@
   $this->request->session();
   $userid = $this->request->session()->read('userid');
   echo $this->Form->create(null,['url' => ['controller'=>'Contents','action' => 'add']]);
-  // echo $this->Form->input("userId",['type'=>'hidden','value'=>$userid]);
+  echo $this->Form->input("userId",['type'=>'hidden','value'=>$userid]);
   echo $this->Form->input("body",['placeholder'=>'What are you doing now?']);
   echo $this->Form->submit("GO");
   echo $this->Form->end();
  ?>
+
+<!-- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ -->
+<!-- A content -->
+<!-- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ -->
+<?php foreach ($contents as $value) { ?>
+  <div class="container">
+    <div class="row">
+      <div class="content-box col-md-6">
+        <p><?php echo $value['username']; ?></p>
+        <p><?php echo $value['body']; ?></p>
+        <p><?php echo $value['favo']; ?></p>
+        <p><?php echo $value['created']; ?></p>
+      </div>
+    </div>
+  </div>
+<?php } ?>
